26ca6cacbc9b6a919ba2383d081c4ef0736abc40
cxx <908382805@qq.com>
2020-01-11 09:25:33 +0800

    add NestExpressApplication

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true, // 设置跨站访问
+    logger: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
